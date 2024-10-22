b99bfe8c6ed3283e1a9f0ba8bb7a9100a076f22a
swaraj kaondal <skaonda@ncsu.edu>
2024-10-21 20:37:39 -0400

    added controllers for admin scripts

--- backend/app/app.py
+++ backend/app/app.py
@@ -1,12 +1,14 @@
 from flask import Flask
 from dotenv import load_dotenv
 from db import connection_pool
-from controllers.faculty import faculty
 from controllers.person import person
+from controllers.faculty import faculty
+from controllers.admin import admin
 
 load_dotenv()
 
 app = Flask(__name__)
 
-app.register_blueprint(faculty)
 app.register_blueprint(person)
+app.register_blueprint(faculty)
+app.register_blueprint(admin)
