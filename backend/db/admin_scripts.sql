5069fcee431f0e05722bb014ddb632af1135971f
Diksha Maurya <mauryadiksha0@gmail.com>
2024-10-23 20:50:24 -0400

    Delete content block

--- backend/db/admin_scripts.sql
+++ backend/db/admin_scripts.sql
@@ -268,7 +268,7 @@ BEGIN
 END#
 -- ----------------------------------------------------------
 
--- 10. hide_content
+-- 10. hide_content_block
 DELIMITER #
 DROP FUNCTION IF EXISTS hide_content#
 CREATE FUNCTION hide_content(content_blk_id INT)
@@ -276,5 +276,24 @@ CREATE FUNCTION hide_content(content_blk_id INT)
     DETERMINISTIC
 BEGIN
   UPDATE Content_Block cont_blk SET Hidden = TRUE WHERE cont_blk.Content_BLK_ID = content_blk_id;
-
+  RETURN 1;
 END#
+
+-- 11. delete_content_block
+DELIMITER #
+DROP FUNCTION IF EXISTS delete_content#
+CREATE FUNCTION delete_content(content_blk_id INT)
+	RETURNS INT
+    DETERMINISTIC
+BEGIN
+  DELETE FROM Answer WHERE Activity_ID IN (SELECT Activity_ID FROM Activity act_id WHERE act_id.Content_BLK_ID = content_blk_id);
+  DELETE FROM Activity act WHERE act.Content_BLK_ID = content_blk_id;
+  DELETE FROM Image img WHERE img.Content_BLK_ID = content_blk_id;
+  DELETE FROM Text_Block txt WHERE txt.Content_BLK_ID = content_blk_id;
+  DELETE FROM Content_Block cont_blk WHERE cont_blk.Content_BLK_ID = content_blk_id;
+  RETURN 1;
+  
+END#
+
+
+
--- backend/db/faculty_queries.sql
+++ backend/db/faculty_queries.sql
@@ -101,3 +101,31 @@ BEGIN
 END; //
 
 DELIMITER ;
+
+-- hide_content_block
+DELIMITER #
+DROP FUNCTION IF EXISTS hide_content#
+CREATE FUNCTION hide_content(content_blk_id INT)
+	RETURNS INT
+    DETERMINISTIC
+BEGIN
+  UPDATE Content_Block cont_blk SET Hidden = TRUE WHERE cont_blk.Content_BLK_ID = content_blk_id;
+  RETURN 1;
+  
+END#
+
+-- delete_content_block
+DELIMITER #
+DROP FUNCTION IF EXISTS delete_content#
+CREATE FUNCTION delete_content(content_blk_id INT)
+	RETURNS INT
+    DETERMINISTIC
+BEGIN
+  DELETE FROM Answer WHERE Activity_ID IN (SELECT Activity_ID FROM Activity act_id WHERE act_id.Content_BLK_ID = content_blk_id);
+  DELETE FROM Activity act WHERE act.Content_BLK_ID = content_blk_id;
+  DELETE FROM Image img WHERE img.Content_BLK_ID = content_blk_id;
+  DELETE FROM Text_Block txt WHERE txt.Content_BLK_ID = content_blk_id;
+  DELETE FROM Content_Block cont_blk WHERE cont_blk.Content_BLK_ID = content_blk_id;
+  RETURN 1;
+  
+END#
