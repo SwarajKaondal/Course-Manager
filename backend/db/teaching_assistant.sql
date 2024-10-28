fe09557e556741b4aff463178f6c3be6f52271bc
Diksha Maurya <mauryadiksha0@gmail.com>
2024-10-28 00:46:59 -0400

    ta procedure change

--- backend/db/teaching_assistant.sql
+++ backend/db/teaching_assistant.sql
@@ -1,26 +1,27 @@
+use zybooks;
+
 -- hide_content_block
-DELIMITER #
-DROP FUNCTION IF EXISTS ta_hide_content#
-CREATE FUNCTION ta_hide_content(content_blk_id INT)
-	RETURNS INT
-    DETERMINISTIC
+DROP PROCEDURE IF EXISTS ta_hide_content;
+DELIMITER //
+CREATE PROCEDURE ta_hide_content(IN content_blk_id INT)
 BEGIN
-  UPDATE Content_Block cont_blk SET Hidden = TRUE WHERE cont_blk.Content_BLK_ID = content_blk_id;
-  RETURN 1;
-END#
+  UPDATE Content_Block cont_blk 
+  SET Hidden = TRUE 
+  WHERE cont_blk.Content_BLK_ID = content_blk_id;
+END ; //
+
+DELIMITER ;
 
 -- delete_content_block
-DELIMITER #
-DROP FUNCTION IF EXISTS ta_delete_content#
-CREATE FUNCTION ta_delete_content(content_blk_id INT)
-	RETURNS INT
-    DETERMINISTIC
+DROP PROCEDURE IF EXISTS ta_delete_content;
+DELIMITER //
+CREATE PROCEDURE ta_delete_content(IN content_blk_id INT)
 BEGIN
   DELETE FROM Answer WHERE Activity_ID IN (SELECT Activity_ID FROM Activity act_id WHERE act_id.Content_BLK_ID = content_blk_id);
   DELETE FROM Activity act WHERE act.Content_BLK_ID = content_blk_id;
   DELETE FROM Image img WHERE img.Content_BLK_ID = content_blk_id;
   DELETE FROM Text_Block txt WHERE txt.Content_BLK_ID = content_blk_id;
-  DELETE FROM Content_Block cont_blk WHERE cont_blk.Content_BLK_ID = content_blk_id;
-  RETURN 1;
-  
-END#
+  DELETE FROM Content_Block cont_blk WHERE cont_blk.Content_BLK_ID = content_blk_id; 
+END; //
+
+DELIMITER ;
